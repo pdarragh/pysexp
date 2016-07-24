74722218a0ba030de70cb456af3aa53d0dd93bf9
Pierce Darragh <pierce.darragh@gmail.com>
2016-07-24 12:09:58 -0600

    Minor functions updates and test coverage
    Used simpler equivalence (`x == y` vs. `x.value == y.value`) and removed superfluous class checking. Also 100% test coverage.

--- pysexp/functions.py
+++ pysexp/functions.py
@@ -8,20 +8,16 @@ def atom(x):
 def eq(x, y):
     if not isinstance(x, Atom) or not isinstance(y, Atom):
         raise ValueError("eq is undefined for non-atomic values")
-    return x.value == y.value
+    return x == y
 
 
 def car(x):
-    if isinstance(x, Atom):
-        raise ValueError("car is undefined for atomic values")
     if not isinstance(x, SExpression):
         raise ValueError("car is defined only for S-Expressions")
     return x.car
 
 
 def cdr(x):
-    if isinstance(x, Atom):
-        raise ValueError("cdr is undefined for atomic values")
     if not isinstance(x, SExpression):
         raise ValueError("cdr is defined only for S-Expressions")
     return x.cdr
---
+++ tests/test_functions.py
@@ -0,0 +1,42 @@
+from pysexp.elements import *
+from pysexp.functions import *
+
+import pytest
+
+
+@pytest.mark.parametrize('val', [
+    'a',
+    1,
+    True,
+    SExpression(Atom('a'), Atom(2)),
+])
+def test_atom(val):
+    a = Atom(val)
+    assert atom(a) == True
+    assert atom(val) == False
+
+
+def test_eq():
+    assert eq(Atom('a'), Atom('a'))
+    assert not eq(Atom('a'), Atom('b'))
+    with pytest.raises(ValueError):
+        eq(Atom('a'), 'a')
+
+
+def test_car():
+    with pytest.raises(ValueError):
+        car(Atom('a'))
+    assert car(SExpression(Atom('a'), Atom('b'))) == Atom('a')
+
+
+def test_cdr():
+    with pytest.raises(ValueError):
+        cdr(Atom('a'))
+    assert cdr(SExpression(Atom('a'), Atom('b'))) == Atom('b')
+
+
+def test_cons():
+    with pytest.raises(ValueError):
+        cons(Atom('a'), 4)
+        cons(3, Atom('b'))
+    assert cons(Atom('a'), Atom('b')) == SExpression(Atom('a'), Atom('b'))
