37438b76bb6321ee6fedd8d574571ee4d8550749
Pierce Darragh <pierce.darragh@gmail.com>
2016-10-05 20:32:31 -0600

    Fixed `cons` implementation
    Now `cons` works. Had to add the ability to "collapse" secondary stuff.

--- pysexp/lists.py
+++ pysexp/lists.py
@@ -9,8 +9,11 @@ class InternalListException(ValueError):
     pass
 
 
+LIST_TYPES = {}
+
+
 class List(SExpression):
-    def __new__(cls, *args):
+    def __new__(cls, *args, **kwargs):
         bases = (cls, )
         inner_type_field = '__inner_type__'
         if len(args) == 0:
@@ -23,19 +26,34 @@ class List(SExpression):
         # Black magic here. We add the type to the global namespace to prevent issues where Python would not identify
         # multiple instances of the same type as actually being the same type (because they would be different 'type'
         # instances with the same attributes -- which is "not the same" to Python).
-        if name not in globals():
-            globals()[name] = type(name, bases, internals)
-        return super().__new__(globals()[name])
+        new_type = type(name, bases, internals)
+        if name not in LIST_TYPES:
+            LIST_TYPES[name] = new_type
+        return super().__new__(new_type)
 
-    def __init__(self, *vals):
+    def __init__(self, *vals, **kwargs):
+        collapse = kwargs.get('collapse', False)
         if len(vals) == 0:
             super().__init__(NIL, NIL)
         elif len(vals) == 1:
             super().__init__(vals[0], NIL)
         else:
-            if not isinstance(vals[1], type(vals[0])):
-                raise InternalListException("type {} does not match internal type {}".format(type(vals[1]), vals[0]))
-            super().__init__(vals[0], List(*vals[1:]))
+            if vals[1] == NIL:
+                super().__init__(vals[0], NIL)
+            else:
+                if collapse:
+                    if not isinstance(vals[1], List):
+                        raise InternalListException("cannot collapse non-List values")
+                    if type(vals[0]) != vals[1].__inner_type__:
+                        raise InternalListException("list type {} does not match list internal type {}".format(
+                            vals[1].__inner_type__, type(vals[0])
+                        ))
+                    super().__init__(vals[0], vals[1])
+                else:
+                    if not isinstance(vals[1], type(vals[0])):
+                        raise InternalListException("type {} does not match list internal type {}".format(
+                            type(vals[1]), type(vals[0])))
+                    super().__init__(vals[0], List(*vals[1:]))
 
     def __car__(self):
         if self.is_empty:
@@ -76,12 +94,12 @@ EMPTY = List()
 
 
 @check_args_instance_of(List)
-def empty(lsexp):
-    return lsexp.is_empty
+def empty(x):
+    return x.is_empty
 
 
 def cons(x, y):
-    return List(x, y)
+    return List(x, y, collapse=True)
 
 
 @optimize_tail_call
